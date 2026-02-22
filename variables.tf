d935bf8e3f4fd587111e528773129113138b4df1
nishantsharan2603 <102582474+nishantsharan2603@users.noreply.github.com>
2026-02-22 21:43:57 +0000

    Update default host pool name in variables.tf

--- variables.tf
+++ variables.tf
@@ -54,7 +54,7 @@ variable "local_admin_username" {
 
 variable "hostpoolname" {
   description = "Host Pool Name to Register Session Hosts"
-  default     = "HP-STD-Win11-SAP-Desktop-EAS"
+  default     = "HP-STD-Win1124h2-sap-Desktop-EAS"
 }
 
 variable "artifactslocation" {
